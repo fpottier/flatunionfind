50f9a6ff8e1c2108b6ad9311472f4947b70e5735
François Pottier <francois.pottier@inria.fr>
2024-12-09 21:45:02 +0100

    Headers.

--- src/FlatUnionFind.mli
+++ src/FlatUnionFind.mli
@@ -1,3 +1,15 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                               FlatUnionFind                                *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*                                                                            *)
+(*       Copyright 2024--2024 Inria. All rights reserved. This file is        *)
+(*       distributed under the terms of the GNU Library General Public        *)
+(*       License, with an exception, as described in the file LICENSE.        *)
+(*                                                                            *)
+(******************************************************************************)
+
 (**This module offers a union-find data structure based on disjoint set
    forests, with path compression and linking by rank. The data structure
    is stored in a flat integer vector. *)
